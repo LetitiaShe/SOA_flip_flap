ba52fed8336e245aff8aaf64b5195a9ed08a5259
LetitiaShe <wehuin1215@gmail.com>
2016-10-02 22:19:32 +0800

    Solved the homework; all tests pass

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -3,12 +3,53 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
+    tsv_lines = []
+    @data = []
+
+    tsv.each_line do |line|
+      tsv_lines << line
+    end
+
+    for i in 0..tsv_lines.length-1
+	    tsv_lines[i] = tsv_lines[i].split("\t")
+    end
+
+    keys = tsv_lines[0]
+    keys.map! {|x| x.chomp}
+    tsv_lines.shift
+
+    tsv_lines.each do |line|
+	    record = Hash.new
+	    keys.each_index {|index| record[keys[index]] = line[index].chomp}
+	    @data.push(record)
+    end
 
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
+    first = @data[0]
+    keys_array = first.keys
+    line = ""
+    for i in 0...keys_array.length
+      if i < keys_array.length-1
+        line = line + keys_array[i] + "\t"
+      else
+        line = line + keys_array[i] + "\n"
+      end
+    end
 
+    @data.each do |record|
+      record.each_with_index do |(key, value), index|
+        if index < record.length-1
+          line << value + "\t"
+        else
+          line << value + "\n"
+        end
+      end
+    end
+    return line
   end
+
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,14 @@
 # Module that can be included (mixin) to take and output Yaml data
+require 'yaml'
+
+module YamlBuddy
+
+  def take_yaml(yaml)
+    @data = YAML.load(yaml)
+  end
+
+  def to_yaml
+    return @data.to_yaml
+  end
+
+end
